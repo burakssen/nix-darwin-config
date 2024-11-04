3f4fafad2f652e8ae797c127790115b81e99c148
burak şen <buraksen7@hotmail.com>
2024-11-04 13:59:20 +0100

    Remove casks

--- flake.nix
+++ flake.nix
@@ -9,9 +9,6 @@
   outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew }:
     let
       configuration = { pkgs, config, ... }: {
-        # List packages installed in system profile. To search by name, run:
-        # $ nix-env -qaP | grep wget
-
         nixpkgs.config.allowUnfree = true;
         environment.systemPackages = [
           pkgs.neovim
@@ -19,19 +16,24 @@
           pkgs.mkalias
           pkgs.cmake
         ];
-
+        
         fonts.packages = [
           (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
         ];
-
+        
         environment.systemPath = [ "/opt/homebrew/bin" ];
-
+        
         homebrew = {
           enable = true;
           brews = [
             "mas"
             "gh"
             "wget"
+            "docker-compose"
+            "tree"
+            "btop"
+            "llvm"
+            "node"
           ];
           casks = [
             "iina"
@@ -49,25 +51,24 @@
             "clion"
             "zed"
             "zen-browser"
-            "${builtins.toString /Users/burakssen/.config/nix/casks/quitme.rb}"
+            "datagrip"
+            "microsoft-teams"
+            #"quitme"
+            "zoom"
+            "sf-symbols"
+            "appcleaner"
           ];
-          masApps = {
-            "WhatsApp" = 310633997;
-            "Telegram" = 747648890;
-            "Xcode" = 497799835;
-            "Microsoft Word" = 462054704;
-            "Microsoft Excel" = 462058435;
-            "Microsoft PowerPoint" = 462062816;
-            "AdGuard" = 1440147259;
-            "Slack" = 803453959;
-          };
+
+          #taps = [
+          #  "burakssen/cask"
+          #];
+       
           onActivation = {
             autoUpdate = true;
             cleanup = "zap";
             upgrade = true;
           };
         };
-
         system.activationScripts.applications.text = let
           env = pkgs.buildEnv {
             name = "system-applications";
@@ -75,32 +76,23 @@
             pathsToLink = "/Applications";
           };
         in
-        pkgs.lib.mkForce ''
-          # Set up applications.
-          echo "setting up /Applications..." >&2
-          rm -rf /Applications/Nix\ Apps
-          mkdir -p /Applications/Nix\ Apps
-          find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-          while read src; do
-            app_name=$(basename "$src")
-            echo "copying $src" >&2
-            ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-          done
-        '';
+          pkgs.lib.mkForce ''
+            # Set up applications.
+            echo "setting up /Applications..." >&2
+            rm -rf /Applications/Nix\ Apps
+            mkdir -p /Applications/Nix\ Apps
+            find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+            while read src; do
+              app_name=$(basename "$src")
+              echo "copying $src" >&2
+              ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+            done
+          '';
 
-        # Auto upgrade nix package and the daemon service.
         services.nix-daemon.enable = true;
-        
-        # Necessary for using flakes on this system.
         nix.settings.experimental-features = "nix-command flakes";
-
-        # Set Git commit hash for darwin-version.
         system.configurationRevision = self.rev or self.dirtyRev or null;
-
-        # Used for backwards compatibility, please read the changelog before changing.
         system.stateVersion = 5;
-
-        # The platform the configuration will be used on.
         nixpkgs.hostPlatform = "aarch64-darwin";
       };
     in
@@ -119,7 +111,6 @@
           }
         ];
       };
-      # Expose the package set, including overlays, for convenience.
       darwinPackages = self.darwinConfigurations."burakssen".pkgs;
     };
 }
